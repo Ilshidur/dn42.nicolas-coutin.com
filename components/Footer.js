33998a3404342f19798a19a5f8e8626c287057e9
Ilshidur <git@miammiam.monster>
2024-07-23 00:19:24 +0200

    feat: finish website

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/Bird_bg_round.svg" alt="Logo" className={styles.logo} />
         <a href="https://nicolas-coutin.com">https://nicolas-coutin.com</a>
         <a href="https://github.com/Ilshidur">https://github.com/Ilshidur</a>
       </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
@@ -8,6 +9,7 @@ export default function Home() {
       <Head>
         <title>AS4242421769</title>
         <link rel="icon" href="/favicon.ico" />
+        <Script src="https://umami.nicolas-coutin.com/script.js" data-website-id="07774cc1-2c10-43f2-a891-dd0f59e4c338" />
       </Head>
 
       <main>
@@ -15,12 +17,12 @@ export default function Home() {
         <p className="description">
           PORYGON network.
         </p>
-        <p>Nodes :</p>
+        <h2>Nodes :</h2>
         <ul>
           <li><code>dn42-fr1.nicolas-coutin.com</code></li>
+          <li>More to come...</li>
         </ul>
-        <p>Contact me on keybase for peering requests : <a href="https://keybase.io/ilshidur">ilshidur</a></p>
-        <p>This site is a work in progress...</p>
+        <p>Contact me on keybase for peering requests : <a href="https://keybase.io/ilshidur">ilshidur</a> — <a href="https://git.dn42.dev/dn42/registry/src/branch/master/data/person/ILSHIDUR-DN42">(find my infos on the dn42 repo)</a></p>
       </main>
 
       <Footer />
