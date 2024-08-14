2ed918356ae786660f67d76d08b31dbd21fc7abf
Nicolas Coutin <ilshidur@gmail.com>
2024-08-14 13:55:02 +0200

    feat: more docs

--- components/Footer.js
+++ components/Footer.js
@@ -7,6 +7,7 @@ export default function Footer() {
         <img src="/Bird_bg_round.svg" alt="Logo" className={styles.logo} />
         <a href="https://nicolas-coutin.com">https://nicolas-coutin.com</a>
         <a href="https://github.com/Ilshidur">https://github.com/Ilshidur</a>
+        <a href="https://as4242421769.ilshidur.dn42">DN42 mirror</a>
       </footer>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,25 @@ export default function Home() {
         <p className="description">
           PORYGON network.
         </p>
+        <ul>
+          <li>172.21.73.128/27</li>
+          <li>fde4:416d:c1::/48</li>
+        </ul>
         <h2>Nodes :</h2>
         <ul>
-          <li><code>dn42-fr1.nicolas-coutin.com</code></li>
+          <li><code>dn42-fr-ren1.nicolas-coutin.com</code> (Rennes, France)</li>
           <li>More to come...</li>
         </ul>
-        <p>Contact me on keybase for peering requests : <a href="https://keybase.io/ilshidur">ilshidur</a> — <a href="https://git.dn42.dev/dn42/registry/src/branch/master/data/person/ILSHIDUR-DN42">(find my infos on the dn42 repo)</a></p>
+        <h2>Services</h2>
+        <ul>
+          <li><a href="https://ping.ilshidur.dn42">https://ping.ilshidur.dn42</a> (work in progress...)</li>
+          <li>More to come...</li>
+        </ul>
+        <h2>Routing policy</h2>
+        <p>(Coming soon.)</p>
+        <h2>Peering requests</h2>
+        <p>Multiprotocol BGP is preferred. Please make sure to implement ROA checking.</p>
+        <p>Contact me on keybase : <a href="https://keybase.io/ilshidur">ilshidur</a> — <a href="https://git.dn42.dev/dn42/registry/src/branch/master/data/person/ILSHIDUR-DN42">(find my infos on the dn42 repo)</a></p>
       </main>
 
       <Footer />
