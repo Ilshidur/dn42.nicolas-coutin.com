4e5155e16499586e889cfbb88fe9163ae266d225
Ilshidur <git+ilshidur@ohcestquoica.com>
2026-04-26 18:44:10 +0200

    feat: umami

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Script from 'next/script'
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        defer
+        src="https://umami.nicolas-coutin.com/script.js"
+        data-website-id="07774cc1-2c10-43f2-a891-dd0f59e4c338"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default Application
